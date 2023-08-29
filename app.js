29a1f4539ade2772e16f8b2a147f48c2d75cac09
tertius Jubber <tjubber@gmail.com>
2023-08-29 15:29:29 +0200

    auth routes added

--- app.js
+++ app.js
@@ -56,4 +56,4 @@ const PORT = process.env.PORT || 3000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))git 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)) 
