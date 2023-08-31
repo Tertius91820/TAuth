3cfc39c5c8795e6f01ebda0f33eac2443fdad94b
tertius Jubber <tjubber@gmail.com>
2023-08-31 14:42:41 +0200

    Headers added

--- routes/auth.js
+++ routes/auth.js
@@ -16,4 +16,37 @@ router.get(
   }
 )
 
+//@desc Logout User
+//@route /auth/logout
+// router.get('/logout', (req,res) => {
+//   req.logout()
+//     res.redirect('/')
+//   })
+
+
+//gpt changes
+// router.get('/logout', async (req, res) => {
+//   try {
+//     req.logout(); // Assuming this function handles the user logout
+
+//     // Redirect to the root path after logout
+//     res.redirect('/');
+//   } catch (err) {
+//     // Handle any errors that occur during logout
+//     console.error('Logout error:', err);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
+
+
+router.get('/logout', (req,res,next) => {
+  req.logout(function(err){
+    if(err){return next(err)}
+    res.redirect('/')
+  })
+  })
+
+
+
+
 module.exports = router
